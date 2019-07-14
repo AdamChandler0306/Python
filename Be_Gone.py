d5fc2c3b6c3e1fc426e6dc94903773fe31551cd4
AdamChandler0306 <AdamChandler0306@gmail.com>
2019-07-14 10:52:14 -0500

    First

---
+++ Be_Gone.py
@@ -0,0 +1,18 @@
+# Why are you still here?
+
+import time
+
+print (time.strftime("%H:%M:%S"))
+
+if 
+
+
+
+$Date = Get-Date | select TimeOfDay
+if ($Date -gt 17:00:00) {Write-host "Go home, it is after 5!"}
+else {Write-host "Get to work!"}
+)
+
+
+all_groups = grp.getgrpall()
+print(all_groups)
